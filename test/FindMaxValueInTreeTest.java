199b0eedfa51d95cec20d8da7fc36fcfc0c6ae82
Javier García <javier.garcia@delonia.com>
2018-05-12 11:01:59 +0200

    Add test for empty Tree

---
+++ test/FindMaxValueInTreeTest.java
@@ -0,0 +1,17 @@
+import static org.hamcrest.CoreMatchers.is;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FindMaxValueInTreeTest {
+    
+    @Test
+    public void findMaxInNullTree(){
+        TreeNode root = null;
+        try {
+            FindMaxValueInTree.findMax(root);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+    
+}
