dc1c3baac12652f2160884116bb0a008bfab3f15
Javier García <javier.garcia@delonia.com>
2018-05-14 09:25:42 +0200

    Add find max in unbalanced tree at left

--- src/FindMaxValueInTree.java
+++ src/FindMaxValueInTree.java
@@ -7,7 +7,7 @@ class FindMaxValueInTree {
             return Math.max(tree.value, Math.max(findMax(tree.left), findMax(tree.right)));
         }
         if(tree.left != null){
-            return Math.max(tree.value, tree.left.value);
+            return Math.max(tree.value, findMax(tree.left));
         }
         if(tree.right != null){
             return Math.max(tree.value, tree.right.value);
