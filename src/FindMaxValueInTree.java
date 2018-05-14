0828ce6f97caaf809fe1512c67316df01de3a205
Javier García <javier.garcia@delonia.com>
2018-05-14 09:11:15 +0200

    Add find max for perfect trees

--- src/FindMaxValueInTree.java
+++ src/FindMaxValueInTree.java
@@ -3,6 +3,9 @@ class FindMaxValueInTree {
         if (tree == null) {
             throw new NullPointerException();
         }
+        if(tree.left != null && tree.right!=null){
+            return Math.max(tree.value, Math.max(findMax(tree.left), findMax(tree.right)));
+        }
         if(tree.left != null){
             return Math.max(tree.value, tree.left.value);
         }
