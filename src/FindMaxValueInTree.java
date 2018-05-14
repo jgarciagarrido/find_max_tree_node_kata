597d0de0418036b83616fa430a05b2cba5c6bad7
Javier García <javier.garcia@delonia.com>
2018-05-14 08:41:21 +0200

    Add find max in tree with left leaf

--- src/FindMaxValueInTree.java
+++ src/FindMaxValueInTree.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 class FindMaxValueInTree {
     static int findMax(TreeNode  tree) {
         if (tree == null) {
             throw new NullPointerException();
+        }
+        if(tree.left != null){
+            return Math.max(tree.value, tree.left.value);
         }
         return tree.value;
     }
