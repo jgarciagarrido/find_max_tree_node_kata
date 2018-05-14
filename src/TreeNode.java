2005dab64095ccac53b539386bf4d02f2e198a57
Javier García <javier.garcia@delonia.com>
2018-05-14 09:01:00 +0200

    Add new constructor to make easy building trees

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -8,5 +8,17 @@ class TreeNode {
         this.left = null;
         this.right = null;
     }
+    
+    TreeNode(int value, int left, int right) {
+        this.value = value;
+        this.left = new TreeNode(left);
+        this.right = new TreeNode(right);
+    }
+    
+    TreeNode(int value, TreeNode left, TreeNode right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
 
 }
